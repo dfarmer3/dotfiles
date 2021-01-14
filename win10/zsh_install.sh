3b9a466da3c818dc961b37be2dd7262d8b5ba7ac
dennisfarmer <dennis.j.farmer2@outlook.com>
2021-01-13 20:44:37 -0500

    windows subsystem dotfile installer

---
+++ win10/zsh_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Run these lines individually
+sudo chsh -s $(which zsh)
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
